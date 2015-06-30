2b6bca34f2bbd08de0764f7be3952298bc3a38c6
sfisher <scott.fisher@ucop.edu>
2015-06-30 12:12:00 -0700

    Temp hack fix for existing problem in datacite engine.

--- app/helpers/datacite/generals_helper.rb
+++ app/helpers/datacite/generals_helper.rb
@@ -1,4 +1,9 @@
 module Datacite
   module GeneralsHelper
+    #this is actually quite bad practice since we shouldn't just assume
+    #another engine is available and reach directly into it and should
+    #separate concerns.  However, I need to get to testing blacklight
+    #and do not want to focus on fixing existing problems.
+    include StashEngine::ResourcesHelper
   end
 end
